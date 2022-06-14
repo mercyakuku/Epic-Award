4559372842a49668c4492fdd6687236c596db65c
mercyakuku <mercyakuku24@gmail.com>
2022-06-14 22:02:43 +0300

    Added location class model

--- awards/models.py
+++ awards/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import User
 import math
 
 # Create your models here.
+class Location(models.Model):
+    country = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=50, null=True)
+    zipcode = models.IntegerField(null=True)
+    address = models.IntegerField(null=True)
 
+    def __str__(self):
+        return self.country
