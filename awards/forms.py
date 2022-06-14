a3cd5c2dde1cb281ef3b1e0ca530d978fb70da6b
mercyakuku <mercyakuku24@gmail.com>
2022-06-14 05:19:46 +0300

    Added the registration form class

--- awards/forms.py
+++ awards/forms.py
@@ -2,3 +2,22 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
+
+class MyRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(MyRegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+
+        user.save()
+
+        return user
