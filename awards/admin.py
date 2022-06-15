735d87bd78993ff7dcc7cdcf9b171749c19618d4
mercyakuku <mercyakuku24@gmail.com>
2022-06-15 05:05:43 +0300

    Registered models in admin file

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Collection,Comment,CommentsLikes,Location,PostLikes,Profile,Post,Rating,Followers,Tags,Technologies
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Rating)
+admin.site.register(Followers)
+admin.site.register(Tags)
+admin.site.register(Technologies)
+admin.site.register(Comment)
+admin.site.register(Collection)
+admin.site.register(PostLikes)
+admin.site.register(CommentsLikes)
+
+
